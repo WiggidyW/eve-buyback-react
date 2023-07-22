55a60541c3bb6b57145b9cab08cd82797639ddd2
Wiggidy <rlr0017@auburn.edu>
2023-07-21 20:31:53 -0500

    remove trailing slash from hash codes

--- src/HashQuery.ts
+++ src/HashQuery.ts
@@ -1,8 +1,10 @@
 const getHashQuery = (): string | undefined => {
   const re = /^[0123456789abcdef]{15,16}\/{0,1}$/;
   const hash = window.location.pathname.substring(1);
-  if (hash !== "" && re.test(hash)) return hash;
-  else return undefined;
+  if (hash !== "" && re.test(hash)) {
+    if (hash.charAt(hash.length - 1) === "/") return hash.slice(0, -1);
+    else return hash;
+  } else return undefined;
 };
 
 const setHashQuery = (hash: string): void => {
